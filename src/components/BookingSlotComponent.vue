57ea54b3636c6e9a716170333e52ddab5dc26fb9
Pol <polcaparrosescudero@gmail.com>
2024-05-24 14:33:53 +0200

    1

--- src/components/BookingSlotComponent.vue
+++ src/components/BookingSlotComponent.vue
@@ -24,19 +24,16 @@ export default {
             this.$emit('update:modelValue', false)
             if (this.bgColor == '') {
                 this.bgColor = 'yellow';
-                this.$emit('update:bgColor', this.bgColor);
                 fetch(`http://localhost:3001/book?bikeId=${this.bikeId}&slotId=${this.$slotId}`, {
                     method: 'GET',
                 }).then(response => response.text())
                     .then(data => {
                         if (data == 'booked' && this.bgColor == 'yellow') {
                             this.bgColor = 'green';
-                            this.$emit('update:bgColor', this.bgColor);
                             this.$emit('update:modelValue', true)
                         }
                         if (data == 'rejected' && this.bgColor == 'yellow') {
                             this.bgColor = 'red';
-                            this.$emit('update:bgColor', this.bgColor);
                         }
 
                     }
